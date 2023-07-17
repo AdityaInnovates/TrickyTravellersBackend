13b37b488cb64be7a2e9fd42adffd2df2f13c279
samadhanuser5 <72184954+itaxhi12@users.noreply.github.com>
2023-07-18 00:12:18 +0530

    fix:authentication in category and blog apis

--- routes/category.route.ts
+++ routes/category.route.ts
@@ -3,11 +3,12 @@ import { CategoryController } from "../controllers";
 import { Router } from "express";
 import validate from "../middlewares/validate";
 import { CategoryValidation } from "../validations";
+import auth from "../middlewares/auth";
 
 const router = Router();
 
-router.get("/", CategoryController.get);
-router.post("/", CategoryController.create);
+router.get("/", auth("category-read"), CategoryController.get);
+router.post("/", auth("category-create"), CategoryController.create);
 router.delete(
   "/:id",
   validate(CategoryValidation.create),
@@ -15,9 +16,10 @@ router.delete(
 );
 router.put(
   "/:id",
+  auth("category-update"),
   validate(CategoryValidation.create),
   CategoryController.update
 );
-router.get("/single/:id", CategoryController.getSingle);
+router.get("/single/:id", auth("category-read"), CategoryController.getSingle);
 
 export default router;
