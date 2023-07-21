807cfe0dea20ad29d26638ca2a5814d0fc6e815f
samadhanuser5 <72184954+itaxhi12@users.noreply.github.com>
2023-07-21 13:22:06 +0530

    rights for events to users and admins

--- routes/event.route.ts
+++ routes/event.route.ts
@@ -3,27 +3,27 @@ import { EventController } from "../controllers";
 import { Router } from "express";
 import validate from "../middlewares/validate";
 import { EventValidation } from "../validations";
-import upload from "../middlewares/multer";
+
 import auth from "../middlewares/auth";
 const router = Router();
 
 router.get("/", EventController.get);
 router.post(
   "/",
-  auth("evenet-update"),
+  auth("event-update"),
 
   validate(EventValidation.create),
   EventController.create
 );
 router.delete(
   "/:id",
-  auth("evenet-delete"),
+  auth("event-delete"),
 
   EventController.deleteDocument
 );
 router.put(
   "/:id",
-  auth("evenet-update"),
+  auth("event-update"),
   validate(EventValidation.update),
   EventController.update
 );
