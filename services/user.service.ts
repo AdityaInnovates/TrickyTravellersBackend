f56b4645ffeffdf91563b0a7fa85465476ca24cc
samadhanuser5 <72184954+itaxhi12@users.noreply.github.com>
2023-08-18 12:24:55 +0530

    login fix

--- services/user.service.ts
+++ services/user.service.ts
@@ -4,7 +4,18 @@ import { StatusCodes } from "http-status-codes";
 import ApiError from "../utils/ApiError";
 
 export const findUser = async (email: string) => {
-  const user = await Model.findOne({ email });
+  const fields = [
+    "name",
+    "password",
+    "email",
+    "role",
+    "active",
+    "ban",
+    "google_id",
+    "remember_token",
+    "profile_photo_path",
+  ];
+  const user = await Model.findOne({ email }).select(fields.join(" "));
 
   if (!user) {
     throw new ApiError(StatusCodes.NOT_FOUND, "Invalid email");
@@ -14,7 +25,9 @@ export const findUser = async (email: string) => {
 
 export const login = async (data: { email: string; password: string }) => {
   const user = await findUser(data.email);
+  console.log(user);
   const pass = await user.isPasswordMatch(data.password);
+  console.log(pass);
   if (!pass) {
     throw new ApiError(StatusCodes.FORBIDDEN, "Invalid Username or password");
   } else return user;
