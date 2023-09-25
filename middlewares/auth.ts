312754765d09baf6334f4fa585d30c20b07e52ab
samadhanuser5 <72184954+itaxhi12@users.noreply.github.com>
2023-09-25 22:56:32 +0530

    changes

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -39,7 +39,6 @@ const verifyCallback = (
 const auth =
   (...requiredRights: any) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.headers);
     return new Promise((resolve, reject) => {
       passport.authenticate(
         "jwt",
